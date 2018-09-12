764edce6035fa068b42faa2438d23f71d0bb734d
Booker Chan <booker0108@gmail.com>
2018-09-12 14:33:13 +0800

    Implement UI of album screen
    Add photo store for future photo retrieve

--- src/screens/AlbumScreen.js
+++ src/screens/AlbumScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
-import {View, Text} from 'react-native'
+import {View, StyleSheet, FlatList, Text, TouchableOpacity} from 'react-native'
+import { Color, Font } from '../styles/CommonStyle';
+import { inject } from 'mobx-react';
 
+@inject('userDetailStore', 'photoStore')
 class AlbumScreen extends Component {
 
     static navigationOptions = {
@@ -8,12 +11,49 @@ class AlbumScreen extends Component {
     }
 
     render () {
+        const {userDetailStore} = this.props;
         return (
-            <View>
-                
+            <View style={styles.container}>
+                <FlatList 
+                    data={userDetailStore.albums}
+                    keyExtractor={item => item.id.toString()}
+                    renderItem={this.renderAlbumItem}
+                    numColumns={2} />
             </View>
         )
     }
+
+    renderAlbumItem = ({item}) => {
+        return (
+            <TouchableOpacity style={styles.ablumContainer} onPress={() => this.onAlbumPress(item)}>
+            <View style={styles.albumInfoContainer}>
+                <Text style={styles.albumThumbnail}>photo_library</Text>
+                <Text numberOfLines={2} style={styles.albumnTitleText}>{item.title}</Text>
+            </View>
+            </TouchableOpacity>
+        )
+    }
+
+    /**
+     * Callback function when pressing on an album
+     * @param {object} album Selected album
+     */
+    onAlbumPress = (album) => {
+
+    }
 }
 
-export default AlbumScreen
+export default AlbumScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Color.bg_color_blue
+    },
+    ablumContainer: {
+        flex: 1
+    },
+    albumInfoContainer: {justifyContent: 'center', alignItems: 'center'},
+    albumThumbnail: {fontFamily: Font.materialIcons, fontSize: 96, color: 'white', padding: 12},
+    albumnTitleText: {padding: 8, color: 'white', fontWeight: 'bold'}
+})
--- src/stores/index.js
+++ src/stores/index.js
@@ -1,9 +1,11 @@
 import UserStore from "./UserStore";
 import UserDetailStore from "./UserDetailStore";
 import CommentStore from "./CommentStore";
+import PhotoStore from "./PhotoStore";
 
 export default {
     userStore: UserStore,
     userDetailStore: UserDetailStore,
-    commentStore: CommentStore
+    commentStore: CommentStore,
+    photoStore: PhotoStore
 }
